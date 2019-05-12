0de34aa199613535d2d8c8a28dee2e4194bb7087
ivanzhangbo <ivanzhangbo@yahoo.co.jp>
2019-05-12 22:30:05 +0900

    first

--- src/api/api.js
+++ src/api/api.js
@@ -6,3 +6,6 @@ let local_host = 'http://127.0.0.1:8000';
 export const login = params => {
   return axios.post(`${local_host}/login/`, params)
 }
+
+//获取商品列表
+export const getGoods = params => { return axios.get(`${local_host}/goods/`, { params: params }) }
--- src/views/index.vue
+++ src/views/index.vue
@@ -12,17 +12,37 @@
                  <router-link :to="'/app/register'">免费注册</router-link>
                 </li>
 
-</ul>
-        index
+        </ul>
+
+        商品数量:{{listData.length}}
+        <hr>
+        <ul>
+            <li v-for="list in listData">{{list.name}}--{{list.shop_price}}</li>
+        </ul>
+
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import { getGoods } from '../api/api'
 
     export default{
         data(){
             return{
+                top_category:'',
+                listData: [],
+                cateMenu: {}, //菜单列表
+                isObject:true,
+                ordering: '-add_time',
+                proNum: 0, //商品数量
+                curLoc: [], //当前位置
+                priceRange: [], //价格区间
+                pricemin: '', //价格最低
+                pricemax: '', //价格最高
+                pageType:'list',
+                searchWord:'',
+                currentCategoryName:''
             }
         },
         methods:{
@@ -32,18 +52,31 @@ import { mapGetters } from 'vuex';
 
                     //跳转到登录
                     this.$router.push({ name: 'login' })
-            //     })
-            //     .catch(function (error) {
-            //       console.log(error);
-            // });
-        },
+             },
+            getListData() {
+                  getGoods({
+                    page: 1, //当前页码
+                    top_category: this.top_category, //商品类型
+                    ordering: this.ordering, //排序类型
+                    pricemin: this.pricemin, //价格最低 默认为‘’ 即为不选价格区间
+                    pricemax: this.pricemax // 价格最高 默认为‘’
+                  }).then((response)=> {
 
+                    this.listData = response.data.results;
+                    this.proNum = response.data.count;
+                  }).catch(function (error) {
+                    console.log(error);
+                  });
+            },
         },
         computed: {
             ...mapGetters({
               // goods_list: 'goods_list',
               userInfo:'userInfo'
             })
+        },
+        created() {
+            this.getListData();
         }
     }
 </script>
